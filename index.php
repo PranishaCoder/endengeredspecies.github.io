89d3f46e73336d5fed5f6412de03205fc74328fe
PranishaCoder <132153187+PranishaCoder@users.noreply.github.com>
2023-06-29 18:41:12 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style1.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+ 
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Saving Wildlife Together!!!</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Letestnews.php">Letest News</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="contact.html">Contact</a>
+        </li>
+
+
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+  	 <div class="carousel-item active">
+      <img src="https://www.mandai.com/content/dam/mandai/about-mandai/Masthead-1920x894-v2.jpg" width="1100" height="500">
+      <div class="carousel-caption">
+      	<strong></strong><h3>COME TO LIFE</h3>
+        <p> For Nature,Wildlife and Our Planet!</p>
+        </strong>
+      </div>   
+    </div>
+
+    <div class="carousel-item ">
+      <img src="https://wallup.net/wp-content/uploads/2019/09/890700-forest-trees-nature-landscape-tree-autumn-deer.jpg" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Eld's Deer</h3>
+        <p>Deer endemic to South Asia and Southeast Asia!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="https://media.istockphoto.com/id/1169290479/photo/male-baya-weaver-attracting-female-bird-in-nesting-colony.jpg?s=612x612&w=0&k=20&c=w1Vc0bfVpy7CfKSIzcx6cpPpI2jq75hx7alYgtfEUZ8=" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Eurasian Tree Sparrow</h3>
+        <p> Sparrow endemic to North America!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="https://webneel.com/daily/sites/default/files/images/daily/02-2013/28-best-wildlife-photography.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Elephas Maximus</h3>
+        <p>Elephant endemic to Asia!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">About Us</h2>
+	</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-12">
+			<img src="https://media.istockphoto.com/id/1177901346/photo/concept-nature-reserve-conserve-wildlife-reserve-tiger-deer-global-warming-food-loaf-ecology.jpg?s=612x612&w=0&k=20&c=pyCkeSvkT-W7IYsXMbKy2gfpSxrHk04M6GzE8p8EsVc="  width="650" height="400" >
+			
+		</div>
+
+		<div class="col-lg-6 col-md-6 col-12">
+			<h2 class="display-4">Saving Wildlife Together!!!</h2>
+			<p class="py-3">The Saving Wildlife Together is , India's largest and most trusted conservation organization, works across the country to unite people's from all walks of life in giving wildlife a voice. We've been on the front lines for wildlife since 2000, fighting for the conservation values that are woven into the fabric of our nation's collective heritage.</p>
+           <p class="py-3">Our vision is to put people first, center equity and justice in everything we do, and combat systems of oppression and other injustices that impede our ability to achieve our mission. By living up to our core values and committing to this vision, the <strong> Saving Wildlife Together</strong> can become an organization that fully welcomes, retains, and celebrates staff, volunteers, and supporters from all backgrounds!!!!</p>
+			<a href="about.php" class="btn btn-success">Check More</a>
+		</div>
+</div>		
+	</div>
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center"><marquee width="60%" direction="right" height="100px">Letest News</marquee></h2>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" >
+ <img class="card-img-top" src="https://www.livemorezone.com/wp-content/uploads/endangered-animals-in-india-live-more-zone-dbs.jpg" alt="Card image" style="height:320px">
+  <div class="card-body">
+    <h4 class="card-title">  Endegered Species Day !! </h4>
+    <p class="card-text"> Endegered Species is almost here!! </p>
+    <a href="https://www.endangered.org/endangered-species-day-is-almost-here/" class="btn btn-primary">Read More</a>
+                    </div>
+               </div>
+		  </div>
+
+
+		  <div class="col-lg-4 col-md-4 col-12">
+				<div class="card" >
+ <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtA-SoPpc9KI6JS-oU7g85V0rhwjYN-bEx9VCwRyXUc0JMzig7z1AeBAVHTmN_WYdhK9o&usqp=CAU" alt="Card image" style="height:295px">
+  <div class="card-body">
+    <h4 class="card-title">Mitigation Techniques!!</h4>
+    <p class="card-text">Mitigation Techniques For Helping Endangered Species!</p>
+    <a href="https://www.endangered.org/mitigation-techniques-for-helping-endangered-species/" class="btn btn-primary">Read More </a>
+                    </div>
+               </div>
+		  </div>
+
+
+		  <div class="col-lg-4 col-md-4 col-12">
+				<div class="card" >
+ <img class="card-img-top" src="https://cdn.britannica.com/96/75396-050-5D1D411B/Mountain-goats-mountains-Olympic-National-Park-Washington.jpg" alt="Card image" style="height:295px">
+  <div class="card-body">
+    <h4 class="card-title">Maine Legislature!!</h4>
+    <p class="card-text">Maine Legislature To Hold Hearing April 5 On Bill To Protect Endangered And Threatened Species Habitat!!</p>
+    <a href="https://www.endangered.org/news-release-maine-legislature-to-hold-hearing-april-5-on-bill-to-protect-endangered-and-threatened-species-habitat/" class="btn btn-primary">Read More</a>
+                    </div>
+               </div>
+		  </div>
+	</div>
+</div>
+</section>
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">#Some of the Endengered Species#</h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<a href="giantpanda.html"><img src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202008/giant_panda_gives_birth_to_bab.jpeg?size=690:388 "  width="450" height="300" ></a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<a href="snowleopard.html">
+				<img src="https://www.wwf.org.uk/sites/default/files/styles/hero_s/public/2016-09/Medium_WW22130.jpg?h=9adcaa5b&itok=SPUs5kqc" width="450" height="300" >
+			     </a>
+			</div>
+		
+			<div class="col-lg-4 col-md-4 col-12">
+				<a href="redpanda.html">
+				<img src="https://images.news18.com/ibnlive/uploads/2022/09/red-panda-16633083703x2.jpg?im=Resize,width=360,aspect=fit,type=normal?im=Resize,width=320,aspect=fit,type=normal" width="450" height="300">
+			</a>
+			</div>
+			<br class=”blank” />
+			<div class="col-lg-4 col-md-4 col-12">
+				<a href="squirrel.html">
+				<img src="https://hips.hearstapps.com/hmg-prod/images/red-squirrel-munching-on-a-hazel-nut-royalty-free-image-1599203407.jpg" width="450" height="300">
+			</a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<a href="tiger.html">
+				<img src="https://images.news18.com/ibnlive/uploads/2021/05/1621518793_tiger-7.jpg?impolicy=website&width=0&height=0" width="450" height="300">
+			    </a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<a href="heron.html">
+				<img src="https://cdn.download.ams.birds.cornell.edu/api/v1/asset/160380671/1200" width="450" height="300">
+			    </a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<a href="bustard.html">
+				<img src="https://thumbs.dreamstime.com/b/great-bustard-19490316.jpg" width="450" height="300">
+			    </a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<a href="Finn'sweaver.html">
+				<img src="https://a-z-animals.com/media/2022/03/shutterstock_99232730-1024x614.jpg" width="450" height="300">
+			    </a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<a href="Jerdon’sCourser.html">
+				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRww9IsJ5NbuipKkDOFzXn1_Nq9AmWCbps6_PwUd7hmIJZGjpZEnD18Ewk75hZVO09aoW0&usqp=CAU" width="450" height="300">
+			    </a>
+			</div>
+		</div>
+	</div>
+	</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">VALUEABLE FEEDBACK</h2>
+	</div>
+	<div class="w-50 m-auto"  >
+		<form action="userinfo2.php" method="post">
+			<div class="form-group">
+				<label>
+					Username
+				</label>
+				<input type="text" name="user" autocomplete="off" class="form-control" >
+				</div>
+
+				<div class="form-group">
+				<label>
+					Email-Id
+				</label>
+				<input type="text" name="email" autocomplete="off" class="form-control" >
+				</div>
+			
+			<div class="form-group">
+				<label>
+					Mobile
+				</label>
+				<input type="text" name="mobile" autocomplete="off" class="form-control" >
+				</div>
+			<div class="form-group">
+				<label>
+					Feedback
+				</label>
+				<textarea class="form-control" name="feedback"></textarea>
+				</div>
+
+				<button type="submit" class="btn btn-success ">Submit</button>
+
+			
+		</form>
+	</div>
+
+	<footer>
+		<p class=" p-3 bg-dark text-white text-center">@SavingWildlifeTogether</p>
+	</footer>
+
+
+
+
+
+
+ <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>	
+	
+</body>
+</html>
